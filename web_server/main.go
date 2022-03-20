b15ad1c647d64a5dd27ddc4081cb2a6ae5d967f2
DDelgadoT <delgadodie99@gmail.com>
2022-03-20 18:09:41 -0500

    Agregando GET a /api

--- web_server/main.go
+++ web_server/main.go
@@ -7,6 +7,7 @@ func main() {
 	server := NewServer("3000")
 	server.Handle("/", "GET", HandleRoot)
 	server.Handle("/api", "POST", server.AddMiddleware(HandleHome, CheckAuth(), Logging()))
+	//server.Handle("/api", "GET", HandleHome)
 	server.Listen()
 
 }
